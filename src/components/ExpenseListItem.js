88b94267d7e22b88dc7a4cd9c3362a0b94c2f3a0
Your Name <you@example.com>
2019-06-02 11:51:15 -0400

    setup formatting for amount and createdAt

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import moment from 'moment';
+import numeral from 'numeral';
 
 //for the button in ExpenseListItem.js props.dispatch messes it all up but in ExpenseListFilter.js I can add or remove "props." from before dispatch with no conscequence, why? It is destructured off of props here so you wouldnt call props of props, I dont know why it will work either way in the other, maybe because there is only ever one dispatch?
 
@@ -10,9 +12,13 @@ import { Link } from "react-router-dom";
         {description}
         </h3>
         </Link>
-        <p>{amount} - {createdAt}</p>
+        <p>
+        {numeral(amount / 100).format('$0,0.00')}
+         - 
+        {moment(createdAt).format('MMMM Do, YYYY')}
+        </p>
     </div>
-);
+); 
 
 
 export default ExpenseListItem;
